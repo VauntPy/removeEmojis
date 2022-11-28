b026eebcc1d79bef606476a84475dbce2e0ca768
vauntpy <vauntpy@gmail.com>
2022-11-28 04:43:17 +0000

    improved

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import emojiArray
 import os
 
 emojis = emojiArray.emojis
+actName = ''
+path  = Path('./RELATIVEPATHTOMAIN.PY').absolute()
+print(path)
 
-path  = Path('./testfolder').absolute()
 os.chdir(path)
 for element in path.iterdir():
+  actName = element.name
   for emoji in emojis:
-    if(emoji in element.name):
-      newName = element.name.replace(emoji, '')
-      element.rename(newName)
+    if(emoji in actName):
+      newName = actName.replace(emoji, '')
+      actName = newName
+  print(element.name)
+  element.rename(actName)
